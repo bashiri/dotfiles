e49ba8f8239ada95d39255b55cc4d24b6dd00d6f
Ali Mohammadpour <aliemohammadpour@gmail.com>
2020-05-21 18:44:37 +0430

    feat: Add Petalinux Prerequisites Packages Installer
    According to Xilinx Petalinux tool user guide (UG1144):
    Table2: Packages and Linux Workstation Environments
    Ubuntu LTS 16.04.x and 18.04.x Packages added.

---
+++ xilinx/prereq-ptlnx.sh
@@ -0,0 +1,30 @@
+sudo apt install \
+        dos2unix \
+        iproute2 \
+        gawk make \
+        net-tools \
+        libncurses5-dev \
+        tftpd zlib1g:i386 \
+        libssl-dev \
+        flex bison \
+        libselinux1 \
+        gnupg \
+        wget \
+        diffstat \
+        chrpath \
+        socat \
+        xterm \
+        autoconf \
+        libtool \
+        tar \
+        unzip \
+        texinfo \
+        zlib1g-dev \
+        gcc-multilib \
+        build-essential \
+        screen \
+        pax \
+        gzip \
+        python2.7 \
+        -y
+
