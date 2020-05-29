11e559865d168ac41fb748f05ca57baf337c6558
Ali Mohammadpour <aliemohammadpour@gmail.com>
2020-05-29 20:14:43 +0430

    feat: Unify common terminal aliases and exports

---
+++ env/life_pathes.sh
@@ -0,0 +1,13 @@
+
+## User Globlal Pathes {{{
+# Location
+export QG_WORKS_PATH=$HOME/work
+export QG_PROJS_PATHS=($HOME/work/projects)
+export QG_TOOLS_PATH=$HOME/tools
+export QG_GITS_PATH=$WORK_PATH/git
+
+# Tools
+export GOPATH=$QG_TOOLS_PATH/go
+
+## }}}
+
---
+++ env/shell_aliases.sh
@@ -0,0 +1,28 @@
+# This file contains aliases for shortcot
+
+## GoToDirectory quick {{{
+# Global Directories
+alias walk2git="cd $QG_GITS_PATH"
+alias walk2work="cd $QG_WORKS_PATH"
+alias walk2proj="cd $QG_PROJS_PATH"
+alias walk2tools="cd $QG_TOOLS_PATH"
+## }}}
+
+## Xilinx Tools {{{
+# begin sources 
+alias envvivado='source $XIL_TOOLS_PATH/Vivado/2019.2/.settings64-Vivado.sh'
+alias envvitis='source $XIL_TOOLS_PATH/Vitis/2019.2/.settings64-Vitis.sh'
+alias envdocnav='source $XIL_TOOLS_PATH/DocNav/.settings64-DocNav.sh'
+alias envmcomposer='source $XIL_TOOLS_PATH/Model_Composer/2019.2/.settings64-Model_Composer.sh'
+alias envxiltools='source $XIL_TOOLS_PATH/Vivado/2019.2/settings64.sh'
+alias envptlnx='source $XIL_TOOLS_PATH/petalinux/2019.2/settings.sh'
+# tools run commands
+alias vitis="GTK_THEME=Adwaita vitis"
+## }}}#
+
+## Eclipse :( {{{
+alias eclipse="GTK_THEM=Adwaita eclipse"
+## }}}
+
+
+
