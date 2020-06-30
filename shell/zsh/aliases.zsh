3225fed6b6a79f71d4ba2a2c867b384452699840
All Mohammadpour <aliemohammadpour@gmail.com>
2020-06-30 14:03:19 +0430

    d

--- shell/zsh/aliases.zsh
+++ shell/zsh/aliases.zsh
@@ -2,10 +2,11 @@
 
 ## GoToDirectory quick {{{
 # Global Directories
-alias walk2git="cd $QG_GITS_PATH"
-alias walk2work="cd $QG_WORKS_PATH"
-alias walk2proj="cd $QG_PROJS_PATH"
-alias walk2tools="cd $QG_TOOLS_PATH"
+alias xxgit="cd $QGITS_PATH"
+alias xxwork="cd $QWORKS_PATH"
+alias xxpro="cd $QG_PROJS_PATH"
+alias xxtools="cd $QTOOLS_PATH"
+alias xxtemp="cd /tmp/"
 ## }}}
 
 ## Xilinx Tools {{{
@@ -17,13 +18,14 @@ alias envmcomposer='source $XIL_TOOLS_PATH/Model_Composer/2019.2/.settings64-Mod
 alias envxiltools='source $XIL_TOOLS_PATH/Vivado/2019.2/settings64.sh'
 alias envptlnx='source $XIL_TOOLS_PATH/petalinux/2019.2/settings.sh'
 # tools run commands
-alias vitis="GTK_THEME=Adwaita vitis"
-alias vivado_hls="GTK_THEME=Adwaita vivado_hls"
+#alias vitis="GTK_THEME=Adwaita vitis"
+#alias vivado_hls="GTK_THEME=Adwaita vivado_hls"
 ## }}}#
 
 ## Eclipse :( {{{
-alias eclipse="GTK_THEME=Adwaita eclipse"
+# alias eclipse="GTK_THEME=Adwaita eclipse"
 ## }}}
 
-
-
+## Commands Short Name Aliases {{{
+alias xo="xdg-open"
+## }}}
