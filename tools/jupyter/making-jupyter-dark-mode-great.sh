ed9cac01cd0e42f22d168e74ae438b8213fa267e
All Mohammadpour <aliemohammadpour@gmail.com>
2020-07-25 18:58:19 +0430

    feat: add jupyter notebook dark theme (monokai)

---
+++ tools/jupyter/making-jupyter-dark-mode-great.sh
@@ -0,0 +1,16 @@
+#!usr/bin/env bash
+# Making Jupyter Dark Mode Great!
+# Reference: https://medium.com/@rbmsingh/making-jupyter-dark-mode-great-5adaedd814db
+
+jt -t monokai \
+        -f fira \
+        -fs 13 \
+        -nf ptsans \
+        -nfs 11 \
+        -N \
+        -kl \
+        -cursw 5 \
+        -cursc r \
+        -cellw 95% \
+        -T
+
