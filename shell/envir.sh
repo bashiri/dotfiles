ed1118802029aa00c2f0c1f21ec48047a4422d2b
Ali Mohammadpour <aliemohammadpour@gmail.com>
2020-10-23 13:04:11 +0330

    feat: Add Environment Variables and Paths

---
+++ shell/envir.sh
@@ -0,0 +1,22 @@
+## User Environment Variables and Paths
+
+## PATHS
+
+# Location {{{
+export QG_WORKS_PATH=NOME
+export QG_TOOLS_PATH=NONE
+export QG_PROGS_PATH=NONE
+export QG_PROJS_PATH=NONE
+export QG_GITHUB_PATH=NONE
+## }}}
+
+# Tools Path {{{
+export XIL_TOOLS_PATH="$QG_TOOLS_PATH/xilinx"
+export GOPATH=$QG_TOOLS_PATH/go
+## }}}
+
+## ProXY Varialbles {{{
+export PROXY_USERNAME="username"
+export PROXY_PASSWORD="password"
+export PROXY_SERVER="server"
+## }}}
